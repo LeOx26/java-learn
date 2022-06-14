942a6257e355e40507a14d3d6dc06b5983e0e310
Leonel Benitez <leoalejandro26@gmail.com>
2022-06-13 23:18:26 -0500

    pokedex init

---
+++ src/main/java/com/example/demo/responses/StringRequest.java
@@ -0,0 +1,14 @@
+package com.example.demo.responses;
+
+public class StringRequest {
+    public String nombre,altura,categoria,habilidad,peso;
+    String n,e,o,s,p;
+    public StringRequest() {
+        this.nombre = n;
+        this.altura = e;
+        this.categoria = o;
+        this.habilidad = s;
+        this.peso = p;
+    }
+
+}
---
+++ src/main/java/com/example/demo/responses/StringResponse.java
@@ -0,0 +1,9 @@
+package com.example.demo.responses;
+
+public class StringResponse {
+
+    public String resultado;
+    public StringResponse(String s) {
+        this.resultado = s;
+    }
+}
